8104c2e60483407e1ce85802ff3a85eb96823770
Lautaro Vazquez <lautivp16@gmail.com>
2023-12-13 18:58:42 -0300

    ftos4

--- carrito.js
+++ carrito.js
@@ -1,34 +1,101 @@
-function mostrarCarrito() {
-    // aca se guarda el carrito
-    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    var vaciarCarritoBtn = document.getElementById('vaciarCarritoBtn')
-    const carritoContainer = document.querySelector('.carrito-container');vaciarCarritoBtn.addEventListener('click', function () {
-        // Vaciar carrito
-        carritoContainer.innerHTML = '';
-      });
-      realizarCompraBtn.addEventListener('click', function () {
-        // vaciar el carrito x compra
-        carritoContainer.innerHTML = '';
-        alert('¡Compra realizada con éxito!');
-      });
-    
-    if (carrito.length === 0) {
-        carritoContainer.innerHTML = '<p>El carrito está vacío.</p>';
-    } else {
-        let total = 0;
-        carritoContainer.innerHTML = '<h2>Productos en el Carrito</h2>';
-        carrito.forEach((producto, index) => {
-            total += producto.precio;
-            carritoContainer.innerHTML += `<p>${index + 1}. ${producto.nombre} - $${producto.precio.toFixed(2)}</p>`;
+let temporizadorNotificacion;
+
+// Recuperar datos del carrito desde el almacenamiento local al cargar la página
+document.addEventListener('DOMContentLoaded', () => {
+    const carritoGuardado = JSON.parse(localStorage.getItem('carrito')) || [];
+    renderizarCarrito(carritoGuardado);
+});
+
+function renderizarCarrito(carrito) {
+    const listaCarrito = document.getElementById('listaCarrito');
+    listaCarrito.innerHTML = '';
+
+    carrito.forEach(producto => {
+        const elementoCarrito = document.createElement('li');
+        elementoCarrito.className = 'producto-carrito';
+
+        const imagenProducto = document.createElement('img');
+        imagenProducto.src = producto.imagen;
+        elementoCarrito.appendChild(imagenProducto);
+
+        const nombreProducto = document.createElement('span');
+        nombreProducto.textContent = producto.nombre;
+        elementoCarrito.appendChild(nombreProducto);
+
+        const precioProducto = document.createElement('span');
+        precioProducto.textContent = `$${producto.precio}`;
+        elementoCarrito.appendChild(precioProducto);
+
+        const botonEliminar = document.createElement('button');
+        botonEliminar.textContent = 'Eliminar';
+        botonEliminar.addEventListener('click', () => {
+            eliminarDelCarrito(elementoCarrito, producto);
         });
-        carritoContainer.innerHTML += `<p><strong>Total: $${total.toFixed(2)}</strong></p>`;
+        elementoCarrito.appendChild(botonEliminar);
+
+        listaCarrito.appendChild(elementoCarrito);
+    });
+}
+
+function vaciarCarrito() {
+    const listaCarrito = document.getElementById('listaCarrito');
+    listaCarrito.innerHTML = '';
+
+    // Vaciar el carrito en el almacenamiento local
+    localStorage.setItem('carrito', JSON.stringify([]));
+}
+
+function eliminarDelCarrito(elementoCarrito, producto) {
+    const listaCarrito = document.getElementById('listaCarrito');
+    listaCarrito.removeChild(elementoCarrito);
+
+    // Obtener el carrito actual del almacenamiento local
+    const carritoActual = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Filtrar el producto eliminado
+    const nuevoCarrito = carritoActual.filter(item => item.nombre !== producto.nombre);
+
+    // Guardar el nuevo carrito en el almacenamiento local
+    localStorage.setItem('carrito', JSON.stringify(nuevoCarrito));
+}
+
+function completarCompra() {
+    const listaCarrito = document.getElementById('listaCarrito');
+    const notificacion = document.getElementById('notificacion');
+    
+    if (listaCarrito.children.length > 0) {
+        // Mostrar notificación solo si hay elementos en el carrito
+        notificacion.style.display = 'block';
+
+        // Ocultar la notificación después de 3 segundos
+        temporizadorNotificacion = setTimeout(() => {
+            notificacion.style.display = 'none';
+        }, 3000);
+
+        // Vaciar el carrito después de completar la compra
+        vaciarCarrito();
+    } else {
+        // Mostrar mensaje de advertencia si el carrito está vacío
+        alert('El carrito está vacío. Añade productos antes de completar la compra.');
     }
 }
 
-// mostrar cuando se cargu la pagina
-mostrarCarrito();
+function realizarCompra() {
+    // Aquí podrías implementar lógica adicional para una compra real
+    const notificacion = document.getElementById('notificacion');
+    notificacion.textContent = 'Compra realizada correctamente. ¡Gracias por tu compra!';
 
+    // Mostrar notificación de compra realizada
+    notificacion.style.display = 'block';
 
+    // Ocultar la notificación después de 3 segundos
+    temporizadorNotificacion = setTimeout(() => {
+        notificacion.style.display = 'none';
+    }, 3000);
+
+    // Vaciar el carrito después de completar la compra
+    vaciarCarrito();
+}
 
 
 
