d51b250b3bf9ae1b811e88e0821e69e14feaa152
Lautaro Vazquez <lautivp16@gmail.com>
2023-12-13 20:59:24 -0300

    botonagregar

--- productos.js
+++ productos.js
@@ -1,121 +1,105 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const darkModeButton = document.getElementById('darkModeButton');
+  const darkModeButton = document.getElementById('darkModeButton');
 
-    // Verificar el estado actual del modo oscuro en localStorage
-    const isDarkMode = localStorage.getItem('darkMode') === 'true';
+  // Verificar el estado actual del modo oscuro en localStorage
+  const isDarkMode = localStorage.getItem('darkMode') === 'true';
 
-    // Aplicar el modo oscuro si es necesario
-    if (isDarkMode) {
-        enableDarkMode();
-    }
+  // Aplicar el modo oscuro si es necesario
+  if (isDarkMode) {
+      enableDarkMode();
+  }
 
-    darkModeButton.addEventListener('click', toggleDarkMode);
+
+  darkModeButton.addEventListener('click', toggleDarkMode);
 });
 
 function toggleDarkMode() {
-    const body = document.body;
+  const body = document.body;
 
-    // Alternar entre modo oscuro y claro
-    if (body.classList.contains('dark-mode')) {
-        disableDarkMode();
-    } else {
-        enableDarkMode();
-    }
+  // Alternar entre modo oscuro y claro
+  if (body.classList.contains('dark-mode')) {
+      disableDarkMode();
+  } else {
+      enableDarkMode();
+  }
 }
 
 function enableDarkMode() {
-    const body = document.body;
-    
-    // Activar modo oscuro
-    body.classList.add('dark-mode');
+  const body = document.body;
+  
+  // Activar modo oscuro
+  body.classList.add('dark-mode');
 
-    // Guardar el estado en localStorage
-    localStorage.setItem('darkMode', 'true');
+  // Guardar el estado en localStorage
+  localStorage.setItem('darkMode', 'true');
 }
 
 function disableDarkMode() {
-    const body = document.body;
-    
-    // Desactivar modo oscuro
-    body.classList.remove('dark-mode');
+  const body = document.body;
+  
+  // Desactivar modo oscuro
+  body.classList.remove('dark-mode');
 
-    // Guardar el estado en localStorage
-    localStorage.setItem('darkMode', 'false');
+  // Guardar el estado en localStorage
+  localStorage.setItem('darkMode', 'false');
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    const productosContainer = document.getElementById('productos-container');
-    const ordenSelect = document.getElementById('orden');
-    const ofertaCheckbox = document.getElementById('oferta');
-  
-   // Cargar productos desde el archivo JSON
-function cargarProductos() {
-    fetch('../productos.json')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Datos de productos:', data);
-        mostrarProductos(data);
-      })
-      .catch(error => console.error('Error al cargar los productos', error));
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const productosContainer = document.getElementById('productos-container');
+  let productos = [];
+
+  // Cargar productos desde el archivo JSON
+  fetch('../productos.json')
+    .then(response => response.json())
+    .then(data => {
+      productos = data;
+      mostrarProductos(productos);
+    });
+
+  function mostrarProductos(productos) {
+    productosContainer.innerHTML = '';
+
+    // Mostrar cada producto
+    productos.forEach(producto => {
+      const productoNode = crearProductoNode(producto);
+      productosContainer.appendChild(productoNode);
+    });
   }
-  
-  
-    // Mostrar productos en el contenedor
-    function mostrarProductos(productos) {
-      productosContainer.innerHTML = '';
-  
-      // Ordenar productos
-      const orden = ordenSelect.value;
-      productos.sort((a, b) => (orden === 'ascendente') ? a.titulo.localeCompare(b.titulo) : b.titulo.localeCompare(a.titulo));
-  
-      // Filtrar productos
-      const soloOfertas = ofertaCheckbox.checked;
-      const productosFiltrados = soloOfertas ? productos.filter(p => p.oferta) : productos;
-  
-      productosFiltrados.forEach(producto => {
-        const productoDiv = document.createElement('div');
-        productoDiv.classList.add('producto');
-  
-        const imagen = document.createElement('img');
-        imagen.src = producto.imagen;
-        imagen.alt = producto.titulo;
-  
-        const titulo = document.createElement('h3');
-        titulo.textContent = producto.titulo;
-  
-        const precio = document.createElement('p');
-        precio.textContent = `Precio: $${producto.precio}`;
-        const botonAgregar = document.createElement('button');
-        botonAgregar.addEventListener('click', () => agregarAlCarrito(producto));
-  
-        productoDiv.appendChild(imagen);
-        productoDiv.appendChild(titulo);
-        productoDiv.appendChild(precio);
-        productosContainer.appendChild(productoDiv);
-      });
-    }
-    //cambio en la selección de orden
-    ordenSelect.addEventListener('change', cargarProductos);
-  
-    // cambio en la opción de mostrar solo ofertas
-    ofertaCheckbox.addEventListener('change', cargarProductos);
-  
-    // Cargar productos al cargar la página
-    cargarProductos,
-    cargarProductos();
-    window.agregarAlCarrito = function (nombreProducto) {
-     // se bisca x producto
-      const productoSeleccionado = productos.find(producto => producto.nombre === nombreProducto);
-  
-      if (productoSeleccionado) {
-        // agregar a carrito
-        console.log(`Producto añadido al carrito: ${productoSeleccionado.nombre}`);
-      }
-    };
-  });
-  
+
+  function crearProductoNode(producto) {
+    const productoNode = document.createElement('div');
+    productoNode.className = 'producto';
+
+    // Crea los elementos HTML para el producto
+    productoNode.innerHTML = `
+      <h3>${producto.titulo}</h3>
+      <p>Precio: $${producto.precio.toFixed(2)}</p>
+      <p>${producto.oferta ? '¡Oferta!' : ''}</p>
+      <button onclick="agregarAlCarrito('${producto.titulo}', ${producto.precio})">Agregar al carrito</button>
+    `;
+
+    return productoNode;
+  }
+
+  // Función para agregar al carrito
+  window.agregarAlCarrito = function (tituloProducto, precioProducto) {
+    // Lógica para agregar el producto al carrito (puedes guardar en local storage, etc.)
+    const carritoItem = { titulo: tituloProducto, precio: precioProducto };
+    agregarAlCarritoHTML(carritoItem);
+  };
+
+  function agregarAlCarritoHTML(item) {
+    // Lógica para agregar el item al HTML del carrito (puedes usar local storage o enviar a una página de carrito, etc.)
+    const carritoContainer = document.getElementById('carrito-container');
+
+    const carritoItemNode = document.createElement('div');
+    carritoItemNode.innerHTML = `<p>${item.titulo} - Precio: $${item.precio.toFixed(2)}</p>`;
+    carritoContainer.appendChild(carritoItemNode);
+  }
+});
